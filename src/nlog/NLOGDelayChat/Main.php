2e9d9e018bd93359d1da216517ed97066de04714
NLOG <sorisem4106@naver.com>
2017-07-04 13:54:59 +0900

    Add files via upload

--- src/nlog/NLOGDelayChat/Main.php
+++ src/nlog/NLOGDelayChat/Main.php
@@ -7,19 +7,36 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
 
-	public $time, $chat, $count;
+	public $time, $chat, $count, $config, $delay;
 	
  	 public function onEnable(){
     	$this->getServer()->getPluginManager()->registerEvents($this, $this);
     	$this->getLogger()->notice("채팅 딜레이 (도배 방지) 플러그인입니다.");
     	$this->getLogger()->notice("Made by NLOG (nlog.kro.kr)");
     	
+    	
+    	@mkdir($this->getDataFolder());
+   	 	$this->config = new Config($this->getDataFolder() . "setting.yml", Config::YAML);
+    	
+    	if (!$this->config->exists('Delay-Time')) {
+    		$this->config->set('Delay-Time', 3);
+    		$this->config->save();
+    	}
+		
+		if (!is_numeric($this->config->get('Delay-Time'))) {
+			$this->config->set('Delay-Time', 3);
+    		$this->config->save();
+		}
+    	
     	$this->count = [ ];
     	$this->time = [ ];
     	$this->chat = [ ];
+		
+		$this->delay = $this->config->get('Delay-Time');
  	 }
  	 
  	 public function onPlayerChat (PlayerChatEvent $ev) {
@@ -27,16 +44,18 @@ class Main extends PluginBase implements Listener{
  	 	$name = $ev->getPlayer()->getName();
  	 	$msg = md5(strtolower(TextFormat::clean($ev->getMessage())));
 		 
-		$this->chat[$name] = [ ];
+		if(!isset($this->chat[$name])){
+			$this->chat[$name] = [];
+		}
  	 	
- 	 	if (!isset($this->time[$name])) {
+ 	 	/*if (!isset($this->time[$name])) {
  	 		$this->time[$name] = time();
-			$this->chat[$name][$msg] = "";
- 	 		return true;
- 	 	}
+			$this->chat[$name][$msg] = "a";
+			return true;
+ 	 	}*/
  	 	
- 	 	if (isset($this->time[$name])  or isset($this->chat[$name][$msg])) {
- 	 		if (time() - $this->time[$name] < 3) {
+ 	 	if (isset($this->time[$name])) {
+ 	 		if (time() - $this->time[$name] <= $this->delay) {
  	 			$ev->setCancelled(true);
  	 			
  	 			$this->time[$name] = time();
@@ -52,19 +71,21 @@ class Main extends PluginBase implements Listener{
  	 				return true;
  	 			}
  	 			
- 	 			$ev->getPlayer()->sendMessage("§o§b[ 알림 ]§7 3초 뒤에 다시 입력해주세요.");
- 	 			return true;
- 	 		}
- 	 		if (isset($this->chat[$name][$msg])) {
- 	 			$ev->setCancelled(true);
- 	 			$ev->getPlayer()->sendMessage("§o§b[ 알림 ]§7 이미 입력하신 채팅 내용입니다.");
+ 	 			$ev->getPlayer()->sendMessage("§o§b[ 알림 ]§7 ".$this->delay."초 뒤에 다시 입력해주세요.");
  	 			return true;
  	 		}
 		}
 		
+ 	 	if (isset($this->chat[$name][$msg])) {
+ 	 		$ev->setCancelled(true);
+ 	 		$ev->getPlayer()->sendMessage("§o§b[ 알림 ]§7 이미 입력하신 채팅 내용입니다.");
+ 	 		return true;
+ 	 	}
+		
 		if (isset($this->count[$name])) {
  	 		unset($this->count[$name]);
  	 	}
+		
  	 	$this->chat[$name][$msg] = "";
 		$this->time[$name] = time();
  	 }
